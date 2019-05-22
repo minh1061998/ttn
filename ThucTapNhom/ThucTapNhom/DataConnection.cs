384e8e58b8682519b9e0f02df485d0e2572eca65
minh1061998 <44441160+minh1061998@users.noreply.github.com>
2019-05-22 14:24:30 +0700

    TTN

---
+++ ThucTapNhom/ThucTapNhom/DataConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucTapNhom
+{
+    class DataConnection
+    {
+        string conStr;
+        public DataConnection()
+        {
+            conStr = "Data Source = DESKTOP-B202809\\SQLEXPRESS; Initial Catalog = ThucTapNhom; Integrated Security = true";
+        }
+
+        public SqlConnection getConnect()
+        {
+            return new SqlConnection(conStr);
+        }
+    }
+}
